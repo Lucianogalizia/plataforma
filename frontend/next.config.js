9a635ff3046208ef2c7147b3d90395749bab95d9
Lucianogalizia <lucianogalizia170@hotmail.com>
2026-02-24 11:38:05 -0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "standalone",
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+    NEXT_PUBLIC_API_URL: "https://dina-backend-848917307732.southamerica-east1.run.app",
   },
 };
 

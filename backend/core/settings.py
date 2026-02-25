8d52e6b2262e3202cbaa346d95055060bce2cf65
Lucianogalizia <lucianogalizia170@hotmail.com>
2026-02-25 18:48:49 -0300

    Create settings.py

---
+++ backend/core/settings.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Plataforma"
+    VERSION: str = "1.0.0"
+
+    # Variables opcionales de entorno (Cloud Run)
+    GOOGLE_CLOUD_PROJECT: str | None = None
+    GCS_BUCKET: str | None = None
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()

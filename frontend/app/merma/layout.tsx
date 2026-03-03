5446d1bb3bc77345dea0371dc5c20025b871f026
Lucianogalizia <lucianogalizia170@hotmail.com>
2026-03-03 10:09:53 -0300

    Create layout.tsx

---
+++ frontend/app/merma/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Sidebar from "@/components/Sidebar";
+
+export default function MermaLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+        {children}
+      </div>
+    </div>
+  );
+}
